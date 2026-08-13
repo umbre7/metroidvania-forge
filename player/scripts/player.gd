8d31581952d10b7b40cb6048167efef6436925fd
umbre7 <arthurbernard2001@gmail.com>
2026-08-13 10:45:20 +0200

    feat: add States related methods in player script

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -1,18 +1,76 @@
 class_name Player extends CharacterBody2D
 
+#region /// State Machine Variables
+var states : Array[PlayerState]
+var current_state : PlayerState : 
+	get : return states.front()
+var previous_state : PlayerState :
+	get : return states[1]
+#endregion
+
+#region /// standart variables
+var direction : Vector2 = Vector2.ZERO
+var gravity : float = 980
+#endregion
+
+
+func _ready() -> void:
+	initialize_states()
+	pass
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	change_state(current_state.handle_input(event))
+	pass
 
 
 func _process(_delta: float) -> void:
-	
+	update_direction()
+	change_state(current_state.process(_delta))
 	pass
 
 
 func _physics_process(_delta: float) -> void:
-	velocity.x = 0
-	if Input.is_action_pressed("left"):
-		velocity.x = -100
-	elif Input.is_action_pressed("right"):
-		velocity.x = 100
-	velocity.y = velocity.y + 980 * _delta
+	velocity.y += gravity * _delta
 	move_and_slide()
+	change_state(current_state.physics_process(_delta))
+	pass
+
+
+func initialize_states() -> void:
+	states = []
+	
+	for c in $States.get_children():
+		if c is PlayerState:
+			states.append(c)
+			c.player = self
+	
+	if states.size() == 0:
+		return
+	
+	for state in states:
+		state.init()
+	
+	change_state(current_state)
+	current_state.enter()
+	pass
+
+
+func change_state(new_state : PlayerState) -> void:
+	if new_state == null || new_state == current_state:
+		return
+	
+	if current_state:
+		current_state.exit()
+	
+	states.push_front(new_state)
+	current_state.enter()
+	states.resize(3)
+	pass
+
+
+func update_direction() -> void:
+	#var prev_direction : Vector2 = direction
+	direction = Input.get_vector("left", "right", "up", "down")
+	
 	pass
