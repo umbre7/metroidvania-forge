a5a6410d26d3c096963584d15990f5600e37e11d
umbre7 <arthurbernard2001@gmail.com>
2026-08-13 14:48:03 +0200

    feat: add jump state script

---
+++ player/states/jump.gd
@@ -0,0 +1,46 @@
+class_name PlayerStateJump extends PlayerState
+
+@export var jump_velocity : float = 450.0
+
+# What happens when the state is initialized ?
+func init() -> void:
+	
+	pass
+
+
+# What happens when we enter this state ?
+func enter() -> void:
+	
+	player.add_debug_indicator(Color.DARK_GREEN)
+	player.velocity.y = -jump_velocity  
+	pass
+
+
+# What happens when we exit this state ?
+func exit() -> void:
+	player.add_debug_indicator(Color.YELLOW)
+	pass
+
+
+# What happens when an nput is pressed ?
+func handle_input(event : InputEvent) -> PlayerState:
+	if event.is_action_released("jump"):
+		player.velocity.y *= 0.5
+		return fall
+	return next_state
+
+
+# What happens each process tick in this state
+func process(_delta : float) -> PlayerState:
+	
+	return next_state
+
+
+# What happens each physics process tick in this state
+func physics_process(_delta : float) -> PlayerState:
+	if player.is_on_floor():
+		return idle
+	elif player.velocity.y >= 0:
+		return fall
+	player.velocity.x = player.direction.x * player.move_speed
+	return next_state
