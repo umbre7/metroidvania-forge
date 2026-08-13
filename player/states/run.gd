d049b1bec5bfc8f202f089c89abfb6acfdee98ec
umbre7 <arthurbernard2001@gmail.com>
2026-08-13 14:48:35 +0200

    feat: add connections to jump and fall states in run and idle states

--- player/states/run.gd
+++ player/states/run.gd
@@ -1,6 +1,8 @@
 class_name PlayerStateRun extends PlayerState
 
 
+
+
 # What happens when the state is initialized ?
 func init() -> void:
 	
@@ -21,16 +23,21 @@ func exit() -> void:
 
 # What happens when an nput is pressed ?
 func handle_input(_event : InputEvent) -> PlayerState:
-	
+	if _event.is_action_pressed("jump"):
+		return jump
 	return next_state
 
 
 # What happens each process tick in this state
 func process(_delta : float) -> PlayerState:
+	if player.direction.x == 0:
+		return idle
 	return next_state
 
 
 # What happens each physics process tick in this state
 func physics_process(_delta : float) -> PlayerState:
-	
+	player.velocity.x = player.direction.x * player.move_speed
+	if player.is_on_floor() == false:
+		return fall
 	return next_state
