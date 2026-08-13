41a06a97fb470749d31dfd860d74d2a775666bb0
umbre7 <arthurbernard2001@gmail.com>
2026-08-13 10:45:37 +0200

    feat: add basic idle state script

---
+++ player/states/idle.gd
@@ -0,0 +1,37 @@
+class_name PlayerStateIdle extends PlayerState
+
+
+# What happens when the state is initialized ?
+func init() -> void:
+	
+	pass
+
+
+# What happens when we enter this state ?
+func enter() -> void:
+	
+	pass
+
+
+# What happens when we exit this state ?
+func exit() -> void:
+	
+	pass
+
+
+# What happens when an nput is pressed ?
+func handle_input(_event : InputEvent) -> PlayerState:
+	
+	return next_state
+
+
+# What happens each process tick in this state
+func process(_delta : float) -> PlayerState:
+	
+	return next_state
+
+
+# What happens each physics process tick in this state
+func physics_process(_delta : float) -> PlayerState:
+	
+	return next_state
