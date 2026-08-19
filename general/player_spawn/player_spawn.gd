b1747b24d096562bcd40dc7fb5ee21213cabe6cf
umbre7 <arthurbernard2001@gmail.com>
2026-08-19 13:54:43 +0200

    feat: add player spawn scene and script

---
+++ general/player_spawn/player_spawn.gd
@@ -0,0 +1,22 @@
+@icon("res://general/icons/player_spawn.svg")
+class_name PlayerSpawn extends Node2D
+
+
+
+func _ready() -> void:
+	visible = false
+	await get_tree().process_frame
+	
+	# Check to see if we already have a player
+	if get_tree().get_first_node_in_group("Player"):
+		# We have a player
+		print("We have a player")
+		return
+	print("No player found")
+	# Instantiate a new instance of our player scene
+	var player : Player = load("res://player/player.tscn").instantiate()
+	get_tree().root.add_child(player)
+	# Position the player scene
+	player.global_position = self.global_position
+	
+	pass
