2b2946da7653c4c54bcc56037c7d192a9dd7bafe
umbre7 <arthurbernard2001@gmail.com>
2026-08-20 18:17:37 +0200

    feat: add global Messages Script

---
+++ 00_global/messages.gd
@@ -0,0 +1,7 @@
+extends Node
+
+@warning_ignore_start("unused_signal")
+signal player_interacted(player : Player)
+signal player_healed(amount : float)
+signal input_hint_changed(hint : String)
+@warning_ignore_restore("unused_signal")
