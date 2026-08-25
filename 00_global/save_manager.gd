80a462eaabfdb05d36fa83e7d75dc5c9179c8da0
umbre7 <arthurbernard2001@gmail.com>
2026-08-25 15:32:42 +0200

    feat: add new methods to save manager global script

--- 00_global/save_manager.gd
+++ 00_global/save_manager.gd
@@ -13,7 +13,7 @@ var persistent_data : Dictionary = {}
 
 
 func _ready() -> void:
-	
+	SceneManager.scene_entered.connect(_on_scene_entered)
 	pass
 
 
@@ -123,3 +123,15 @@ func get_file_name(slot : int) -> String:
 
 func save_file_exists(slot: int) -> bool:
 	return FileAccess.file_exists(get_file_name(slot))
+
+
+func is_area_discovered(scene_uid : String) -> bool:
+	return discovered_areas.has(scene_uid)
+
+
+func _on_scene_entered(scene_uid : String) -> void:
+	if discovered_areas.has(scene_uid):
+		return
+	else:
+		discovered_areas.append(scene_uid)
+	
